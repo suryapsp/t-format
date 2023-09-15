83ed3350dc09398344ac648b650f638bc1b006af
suryapsp <techie-human@protonmail.com>
2023-09-15 18:55:56 +0530

    react routers

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextInput from "./components/TextInput";
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import About from "./components/About";
 
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
 
   const toggleMode = () => {
-    if (mode === 'dark') {
-      setMode('light') 
-      document.body.style.backgroundColor = 'white'
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "gray";
     }
-    else{
-      setMode('dark')
-      document.body.style.backgroundColor = 'gray'
-    }
-  }
+  };
 
   return (
     <>
-      <Navbar title="TFormat" mode={mode} toggleMode = {toggleMode}/>
-      <div className="container my-3">
-        <TextInput heading="Enter Your Text" mode={mode} />
-      </div>
+      <Router>
+        <Navbar title="TFormat" mode={mode} toggleMode={toggleMode} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/" element={<TextInput heading="Enter Your Text" mode={mode} />}/>
+            <Route path="/about" element={<About />}/>
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,7 @@
 import React from 'react'
 
-export default function 
-() {
+export default function About() {
   return (
-    <div>
-        
-    </div>
+    <div>About</div>
   )
 }
